840bff7c727680bf8367d10601bdf5b44d3a82c9
vaghdeviS <vaghdevi.sunkari@gmail.com>
2022-10-02 22:47:08 -0400

    Array list

---
+++ src/Arraylistex.java
@@ -0,0 +1,26 @@
+
+
+import java.util.ArrayList;
+
+public class Arraylistex {
+	 public static void main(String args[])
+		{
+		 
+		 ArrayList<String> list = new ArrayList<String>();
+		 list.add("Sri");
+		 list.add("Ram");
+		 list.add("Raj");
+		 list.add("Ravi");
+		 list.add("Veda");
+		 
+		 System.out.println(list.size());
+		 
+		 for(String x:list)
+			 System.out.println(x);
+		 
+		 
+		 
+		 
+		 
+		}
+}
